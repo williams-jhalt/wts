2f0dd2b03a1ec6ae466c7ec90488b1ca736a3b7f
johnh <johnh@DESKTOP-B7JMS42.hq.williamstradingco.com>
2016-08-01 08:37:59 -0400

    consolidated invoices

--- src/AppBundle/Service/ErpOneOrderService.php
+++ src/AppBundle/Service/ErpOneOrderService.php
@@ -298,7 +298,7 @@ class ErpOneOrderService {
 
         $ch = curl_init();
 
-        $oeHeadResponse = $this->_erp->read("FOR EACH oe_head NO-LOCK WHERE company_oe = '{$this->_erp->getCompany()}' AND opn = yes AND consolidated_order = yes");
+        $oeHeadResponse = $this->_erp->read("FOR EACH oe_head NO-LOCK WHERE company_oe = '{$this->_erp->getCompany()}' AND invc_date > today - 7 AND consolidated_order = yes");
 
         $this->_loadFromErp($oeHeadResponse);
 
@@ -310,7 +310,7 @@ class ErpOneOrderService {
 
         $openConsInv = $this->_em->getRepository('AppBundle:Invoice')
                 ->createQueryBuilder('o')
-                ->where('o.open = true')
+                ->where('SIZE(o.items) = 0')
                 ->andWhere('o.consolidated = true')
                 ->andWhere('o.updatedOn < :timeAgo OR o.updatedOn is null')
                 ->setParameter('timeAgo', new DateTime('now -15 minutes'))
